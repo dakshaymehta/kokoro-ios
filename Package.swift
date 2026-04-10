411c4e8b6c813fbf2dd16bf7cb9b5362ab804e09
Dakshay Mehta <dakshay.rio@gmail.com>
2026-04-10 11:50:09 -0700

    fix: point MisakiSwift at fork with mlx-swift 0.30.6+ pin

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
   dependencies: [
     .package(url: "https://github.com/ml-explore/mlx-swift", from: "0.30.6"),
     // .package(url: "https://github.com/mlalma/eSpeakNGSwift", from: "1.0.1"),
-    .package(url: "https://github.com/mlalma/MisakiSwift", exact: "1.0.6"),
+    .package(url: "https://github.com/dakshaymehta/MisakiSwift", from: "1.0.7"),
     .package(url: "https://github.com/mlalma/MLXUtilsLibrary.git", exact: "0.0.6")
   ],
   targets: [
